042f68171958a3b48ef205a49593f6107bd0f8f8
Masaya Heywood <mkh429@nyu.edu>
2020-11-09 16:45:42 -0500

    Added a category counter function

---
+++ cat_counter_function.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  9 04:09:56 2020
+
+@author: Masaya Heywood
+"""
+
+import numpy as np
+import pandas as pd
+import scipy.stats as sp
+import sklearn.metrics as sk
+
+dummyList = [3, 4, 5, 8, 5]
+dummy2DList = [(0,50), (10,5), (5,10)]
+dummyNumpy = np.array(dummyList)
+dummy2DNumpy = np.array(dummy2DList)
+dummyRand2DNumpy = np.random.rand(100, 2) 
+
+def catCount (numberSet):    
+    """
+    Function by Masaya Heywood
+    Takes in a 1D numpy array or list (numberSet) and returns a 2D array detailing the entry and count of entries (Unique value, amount of values).
+    """
+    from collections import Counter
+
+    output = []
+    uniqueValue = list(Counter(numberSet).values())
+    valueCount = list(Counter(numberSet).keys())
+    Counter(numberSet).keys()
+    
+                       
+    for each in range(len(uniqueValue)):
+        output.append(1)
+        output[each] = (valueCount[each], uniqueValue[each])
+        
+            
+    return (sorted(output))
+
+print(catCount(dummyList))
